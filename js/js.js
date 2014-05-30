5c51899439046f7e68c884216fd70895e7702004
luisgreydot <Greydot@Luisweb-3.local>
2014-05-30 18:45:13 -0500

    Efecto Parallax
    Se hizo la prueba del efecto parallax y ya funciona en una sola imagen.

---
+++ js/js.js
@@ -0,0 +1,18 @@
+$(document).ready(function ($){
+	//Cargamos la función Stellar que nos dará el efecto parallax.
+	$(function(){
+      $(window).stellar(); 
+  	});
+
+  	//Activa el Scroll Horizontal
+  	$(function() {
+        $('nav a').bind('click',function(event){
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollLeft: $($anchor.attr('href')).offset().left
+            }, 2000);
+            event.preventDefault();
+        });
+    });
+
+});
