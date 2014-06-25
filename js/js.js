884665788d1b24f2cec57aa7bb3f1fac8473b000
luisgreydot <Greydot@Luisweb-3.local>
2014-06-25 17:54:03 -0500

    Instalación de galerías Jquery
    Se terminó galería lightbox y se agregó al index para hacer pruebas. Ya
    funcionan sin problemas en Chrome.

--- js/js.js
+++ js/js.js
@@ -1,24 +1,101 @@
 $(document).ready(function ($){
-	//Cargamos la función Stellar que nos dará el efecto parallax.
+	//Carga la función Stellar que nos dará el efecto parallax.
 	$(function(){
       $(window).stellar(); 
-  	});
+  });
 
-  	//Activa el Scroll Horizontal
-  	$(function() {
-        $('nav a').bind('click',function(event){
-            var $anchor = $(this);
-            $('html, body').stop().animate({
-                scrollLeft: $($anchor.attr('href')).offset().left
-            }, 2000);
-            event.preventDefault();
-        });
-    });
-
-    //Cargamos el scroll del cuadro de clientes
-    $(function(){
-      $('.nano').nanoScroller({
-        preventPageScrolling: true
+	//Activa el Scroll Horizontal
+	$(function() {
+      $('nav a').bind('click',function(event){
+          var $anchor = $(this);
+          $('html, body').stop().animate({
+              scrollLeft: $($anchor.attr('href')).offset().left
+          }, 2000);
+          event.preventDefault();
       });
+  });
+
+  //Carga el scroll del cuadro de clientes
+  $(function(){
+    $('.nano').nanoScroller({
+      preventPageScrolling: true
     });
+  });
+
+  //Carga el efecto lightbox de las galerías
+  $("#nuestro_trabajo_print").click(function() {
+    $.fancybox.open([
+      {
+        href : 'img/1_b.jpg',
+        title : 'Proyecto 1'
+      }, {
+        href : 'img/2_b.jpg',
+        title : 'Proyecto 2'
+      }, {
+        href : 'img/3_b.jpg',
+        title : 'Proyecto 3'
+      }
+    ], {
+      helpers : {
+        thumbs : {
+          width: 75,
+          height: 50
+        }
+      }
+    });
+  });
+  $("#nuestro_trabajo_tv").click(function() {
+    $.fancybox.open([
+      {
+        href : 'img/1_b.jpg',
+        title : 'Proyecto 1'
+      }, {
+        href : 'img/2_b.jpg',
+        title : 'Proyecto 2'
+      }, {
+        href : 'img/3_b.jpg',
+        title : 'Proyecto 3'
+      }
+    ], {
+      helpers : {
+        thumbs : {
+          width: 75,
+          height: 50
+        }
+      }
+    });
+  });
+  $("#nuestro_trabajo_radio").click(function() {
+    $.fancybox.open([
+      {
+        href : 'img/1_b.jpg',
+        title : 'Proyecto 1'
+      }, {
+        href : 'img/2_b.jpg',
+        title : 'Proyecto 2'
+      }, {
+        href : 'img/3_b.jpg',
+        title : 'Proyecto 3'
+      }
+    ], {
+      helpers : {
+        thumbs : {
+          width: 75,
+          height: 50
+        }
+      }
+    });
+  });
+
+  //Activa el scroll del cuadro de clientes
+  $(function(){
+    $('.nano').nanoScroller({
+      preventPageScrolling: true
+    });
+  });
+
+  //Activa el botón de cerrado y abierto
+  $("#toggle_clientes").click(function(){
+    $("#clientes_lista").toggle("showOrHide");
+  });
 });
